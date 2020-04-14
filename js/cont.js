5b4de7bdb2c480abf62bdd34b6e65b6bb48792c8
thaer123 <your email here in single quotes>
2020-04-14 17:32:38 +0300

    this is my first java

---
+++ js/cont.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var customerName = prompt("Please enter your name", "<name goes here>");
+
+if (customerName!= null) {
+
+    document.getElementById("welcome").innerHTML =
+
+    "Hello " + customerName + "! How are you today?";
+
+}
+
+
+
+
+    
