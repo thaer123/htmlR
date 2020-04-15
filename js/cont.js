d84dbc1edb0985e23b78a780ec8fbe1910416a7c
thaer123 <your email here in single quotes>
2020-04-15 23:50:44 +0300

    adding function and loops

--- js/cont.js
+++ js/cont.js
@@ -14,21 +14,37 @@ function login(){
 
 function foodOrder(){
     var foodtype=prompt('whats your fav food:');
-    var photo;
+    var photo='';
+
+    while (foodtype!=='mansaf' && foodtype!=='falafel' && foodtype!=='shawerma') {
+
+       foodtype=prompt('please chose mansaf or shawerma or falafel');
+        
+    }
+    var numberOfOrder=prompt('haw many');
+    console.log(numberOfOrder);
+    
+    for (var i = 0; i< numberOfOrder; i++) {
+       
     if (foodtype==='mansaf') {
-        photo='<https://migrationology.com/wp-content/uploads/2016/04/mansaf-jordanian-food.jpg">';
+        photo=photo+'<img src="https://migrationology.com/wp-content/uploads/2016/04/mansaf-jordanian-food.jpg">';
 
         
     } else if (foodtype==='falafel') {
-        photo='<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRNCBvzqVUTtPRnqtRh5agxHcraTtXHJlOxA8eypYP8HUSPDNTo&usqp=CAU">';
+         photo=photo+'<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRNCBvzqVUTtPRnqtRh5agxHcraTtXHJlOxA8eypYP8HUSPDNTo&usqp=CAU">';
 
     } else if (foodtype==='shawerma') {
 
-        photo='<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRDRyrq8TdpdG7nW_1ddNaOlxRBZaKhTrHdCqjCc2zlkmI1fHHj&usqp=CAU">';
-    } else {
-        photo='please chose mansaf or shawerma or falafel ';
-        
-    }
+        photo=photo+'<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRDRyrq8TdpdG7nW_1ddNaOlxRBZaKhTrHdCqjCc2zlkmI1fHHj&usqp=CAU">';
+   } 
+   //else {
+    //    photo='please chose mansaf or shawerma or falafel ';
+}   
+
+   
+    
+
     return photo;
+    
     //document.write(foodOrder());
 }
